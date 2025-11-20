7e8c6978dba7860714d69cca82b5cbf15f69453d
Tan <mondal.tanmoy@synergicsoftek.com>
2025-11-20 15:47:31 +0530

    comment added [skip ci]

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -2,6 +2,8 @@ import { string } from 'prop-types';
 
 import './HelpBox.css';
 
+//This is a test comment
+
 function HelpBox({ title, text }) {
   return (
     <article className="help-box-present">
