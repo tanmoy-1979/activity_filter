5756a70c04f9935c943424729be37047144a54f6
Tan <mondal.tanmoy@synergicsoftek.com>
2025-11-20 15:32:58 +0530

    chaged workflow

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -4,7 +4,7 @@ import './HelpBox.css';
 
 function HelpBox({ title, text }) {
   return (
-    <article className="help-box">
+    <article className="help-box-present">
       <h2>{title}</h2>
       <p>{text}</p>
     </article>
